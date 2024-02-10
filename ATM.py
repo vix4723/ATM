d43b85b4d34bfc4d5d24dd0d8acff119465f363f
Vix <vac3wxd@virginia.edu>
2024-02-10 06:23:10 +0100

    New Updates
    Added more options and fixed some conditions

--- ATM.py
+++ ATM.py
@@ -1,6 +1,7 @@
 attempts = 3
 userPin = 1234
 userPreference = 0
+totalMoney = 0
 
 print(
 '''
@@ -11,9 +12,10 @@ print(
 ************************************************************
 ''')
 
-userPreference = int(input(
+print(
 '''1: Access the System
-2: Exit'''))
+2: Exit''')
+userPreference = int(input())
 
 if userPreference == 2:
     SystemExit
@@ -21,25 +23,45 @@ if userPreference == 2:
 if userPreference == 1:
     while attempts != 0:
         attemptPin = int(input("Please Enter Your 4 Digit Pin Number: "))
-        if userPin != attemptPin:
+        if attemptPin != userPin:
             attempts -= 1
-            print('Wrong Pin Number, you have ' + attempts + ' attempts left')
+            print('Wrong Pin Number, you have ' + str(attempts) + ' attempts left')
             print('Try Again')
         else:
-            userChoice = input(
-                '''
-                D) Deposit        
-                W) Withdraw
-                ''')
-            if userChoice == "D" or 'd':
+            print('You have successfully accessed Adeu\'s ATM system')
+            print('''
+
+Please Select an Option                  
+
+D) Deposit        
+W) Withdraw
+T) Check Total Balance
+X) Change Pin Number
+E) Exit
+''')
+            userChoice = input()
+            if userChoice == "D" or userChoice == "d":
                 userDeposit = input('Enter the Total Amount You Would like to Deposit: ')
+                totalMoney += userDeposit
                 print('$', userDeposit, ' have been added to your account')
-            elif userChoice == 'W' or 'w':
-                userDeposit = input('Enter the Total Amount You Would like to Withdraw: ')
+            elif userChoice == "W" or userChoice == "w":
+                userWithdrawn = input('Enter the Total Amount You Would like to Withdraw: ')
+                totalMoney -= userWithdrawn
                 print('$', userDeposit, ' have been withdrawn from your account')
-        UserHasFinished = input('Would You Like to Continue? Y/N: ')
-        if UserHasFinished == 'Y':
-            continue
-        elif UserHasFinished == 'N':
-            print("Thank you for your time")
-            break
+            elif userChoice == "T" or userChoice == "t":
+                print("You have a total amount of $" + str(totalMoney) + " in your bank account")
+            elif userChoice == "X" or userChoice == "x":
+                newPin = input('Please enter your new Pin Number: ')
+                confirmedPin = input('Please confirm your Pin Number: ')
+                userPin = newPin
+                if userPin == newPin:
+                    print('Your Pin has been successfully updated')
+            elif userChoice == "E" or userChoice == "e":
+                SystemExit
+            UserHasFinished = input('Would You Like to Continue? Y/N: ')
+            if UserHasFinished == 'Y':
+                continue
+            elif UserHasFinished == 'N':
+                print("Thank you for your time!")
+                print('Have a Nice Day')
+                break
